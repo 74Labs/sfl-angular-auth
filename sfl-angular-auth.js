76f33cd1896d635e82c03a9f7aed40e90e14ff43
Tom Stanczyk <tomasz.stanczyk@gmail.com>
2016-05-04 00:54:48 +0200

    Update sfl-angular-auth.js

--- sfl-angular-auth.js
+++ sfl-angular-auth.js
@@ -47,11 +47,11 @@ angular.module('sfl.auth', [
     };
 })
 
-.service('sflAuth', function ($log, sflSession, sflAuthLoopBack, $rootScope, $state, $filter) {
+.service('sflAuth', function ($log, sflSession, sflAuthDummy, $rootScope, $state, $filter) {
 
     var self = this;
 
-    self.provider = sflAuthLoopBack;
+    self.provider = sflAuthDummy;
 
     self.redirections = {
         loginState: 'login',
@@ -178,4 +178,53 @@ angular.module('sfl.auth', [
             });
         }
     };
+});
+
+angular.module('sfl.auth.dummy', [])
+
+.service('sflAuthDummy', function ($log) {
+
+    var self = this;
+
+    self.currentUser = null;
+
+    self.register = function (user, successCallback, errorCallback) {
+        $log.info('SFL dummy auth', 'REGISTER');
+    };
+
+    self.reset = function (email, successCallback, errorCallback) {
+        $log.info('SFL dummy auth', 'RESET');
+    };
+
+    self.confirm = function (user, successCallback, errorCallback) {
+        $log.info('SFL dummy auth', 'CONFIRM');
+    };
+
+    self.login = function (credentials, successCallback, errorCallback) {
+        $log.info('SFL dummy auth', 'LOGIN');
+        self.currentUser = { email: 'dummy@localhost.dev', nickName: 'Dummy User' };
+        if (successCallback) successCallback(self.currentUser);
+    };
+
+    self.logout = function (successCallback, errorCallback) {
+        $log.info('SFL dummy auth', 'LOGOUT');
+        self.currentUser = null;
+    };
+
+    self.isAuthenticated = function () {
+        return self.currentUser !== null;
+    };
+
+    self.isAuthorized = function () {
+        return self.isAuthenticated();
+    };
+
+    self.restore = function (successCallback) {
+        $log.info('SFL dummy auth', 'RESTORE');
+    };
+
+    self.getCurrentUser = function () {
+        return self.currentUser;
+    };
+
 });
